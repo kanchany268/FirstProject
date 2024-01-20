ada5e0db82c4380f0c45bccec181a358bea6b353
Chiplink Computer <Chiplink Computer@DESKTOP-KQ7RV25>
2024-01-20 20:47:43 +0530

    Added Changes

--- SeleniumProject-01/src/test/java/swaglabsTestPage/LoginTestRun.java
+++ SeleniumProject-01/src/test/java/swaglabsTestPage/LoginTestRun.java
@@ -7,12 +7,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import addtocart.AddtocartIteams;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 import logoutPage.logoutpage;
 import menuitemsPage.MeanuItems;
@@ -82,6 +81,8 @@ public class LoginTestRun {
 		}
 		
 	}
+		
+		
 	}
 	
 	}
